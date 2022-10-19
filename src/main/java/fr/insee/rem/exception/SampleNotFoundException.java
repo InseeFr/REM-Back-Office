7b780c27fb6187c6d853cea775045dbeff839944
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2022-10-19 18:25:29 +0200

    refactor: add Exceptions Handlers

---
+++ src/main/java/fr/insee/rem/exception/SampleNotFoundException.java
@@ -0,0 +1,14 @@
+package fr.insee.rem.exception;
+
+public class SampleNotFoundException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3783013039351734977L;
+
+    public SampleNotFoundException(Long id) {
+        super(String.format("Sample [%s] doesn't exist", id));
+    }
+
+}
---
+++ src/main/java/fr/insee/rem/exception/SurveyUnitNotFoundException.java
@@ -0,0 +1,14 @@
+package fr.insee.rem.exception;
+
+public class SurveyUnitNotFoundException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -6392109505374844497L;
+
+    public SurveyUnitNotFoundException(Long id) {
+        super(String.format("SurveyUnit [%s] doesn't exist", id));
+    }
+
+}
