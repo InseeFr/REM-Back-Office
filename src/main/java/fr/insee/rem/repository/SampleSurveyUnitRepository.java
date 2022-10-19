5c6d3528c5531fd9df077998bd22ea115a70fba7
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2022-10-19 18:30:52 +0200

    refactor: use JpaRepository

--- src/main/java/fr/insee/rem/repository/SampleSurveyUnitRepository.java
+++ src/main/java/fr/insee/rem/repository/SampleSurveyUnitRepository.java
@@ -2,14 +2,14 @@ package fr.insee.rem.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.insee.rem.entities.Sample;
 import fr.insee.rem.entities.SampleSurveyUnit;
 
 @Repository
-public interface SampleSurveyUnitRepository extends CrudRepository<SampleSurveyUnit, Long> {
+public interface SampleSurveyUnitRepository extends JpaRepository<SampleSurveyUnit, Long> {
 
     List<SampleSurveyUnit> findBySample(Sample sample);
 
--- src/main/java/fr/insee/rem/repository/SurveyUnitRepository.java
+++ src/main/java/fr/insee/rem/repository/SurveyUnitRepository.java
@@ -2,15 +2,15 @@ package fr.insee.rem.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.insee.rem.entities.Sample;
 import fr.insee.rem.entities.SurveyUnit;
 
 @Repository
-public interface SurveyUnitRepository extends CrudRepository<SurveyUnit, Long> {
+public interface SurveyUnitRepository extends JpaRepository<SurveyUnit, Long> {
 
     @Query("SELECT su FROM SurveyUnit su LEFT JOIN SampleSurveyUnit ssu ON (ssu.surveyUnit = su) WHERE ssu.sample = ?1")
     List<SurveyUnit> findAllBySample(Sample sample);
