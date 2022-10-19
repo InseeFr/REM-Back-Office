f04751efd1ac766e3fa90715b8c25f1dde39c730
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2022-10-19 18:32:53 +0200

    refactor: use Exceptions

--- src/main/java/fr/insee/rem/service/SampleService.java
+++ src/main/java/fr/insee/rem/service/SampleService.java
@@ -5,16 +5,23 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import fr.insee.rem.entities.Response;
+import fr.insee.rem.entities.Sample;
 import fr.insee.rem.entities.SurveyUnit;
+import fr.insee.rem.exception.CsvFileException;
+import fr.insee.rem.exception.SampleNotFoundException;
 
 public interface SampleService {
 
-    Response addSampleFromCSVFile(Long sampleId, MultipartFile sampleFile);
+    Response addSampleFromCSVFile(Long sampleId, MultipartFile sampleFile) throws SampleNotFoundException, CsvFileException;
 
-    Response deleteSample(Long sampleId);
+    Response deleteSample(Long sampleId) throws SampleNotFoundException;
 
-    List<SurveyUnit> getSample(Long sampleId);
+    List<SurveyUnit> getSurveyUnitsBySample(Long sampleId);
 
     Response putSample(String label);
+
+    Sample getSample(Long sampleId) throws SampleNotFoundException;
+
+    List<Sample> getAllSamples();
 
 }
--- src/main/java/fr/insee/rem/service/impl/SampleServiceImpl.java
+++ src/main/java/fr/insee/rem/service/impl/SampleServiceImpl.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -25,16 +24,16 @@ import fr.insee.rem.entities.Response;
 import fr.insee.rem.entities.Sample;
 import fr.insee.rem.entities.SampleSurveyUnit;
 import fr.insee.rem.entities.SurveyUnit;
+import fr.insee.rem.exception.CsvFileException;
+import fr.insee.rem.exception.SampleNotFoundException;
 import fr.insee.rem.repository.SampleRepository;
 import fr.insee.rem.repository.SampleSurveyUnitRepository;
 import fr.insee.rem.repository.SurveyUnitRepository;
 import fr.insee.rem.service.SampleService;
 import fr.insee.rem.service.SurveyUnitService;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Transactional
-@Slf4j
 public class SampleServiceImpl implements SampleService {
 
     @Autowired
@@ -50,15 +49,13 @@ public class SampleServiceImpl implements SampleService {
     SurveyUnitRepository surveyUnitRepository;
 
     @Override
-    public Response addSampleFromCSVFile(Long sampleId, MultipartFile sampleFile) {
+    public Response addSampleFromCSVFile(Long sampleId, MultipartFile sampleFile) throws SampleNotFoundException, CsvFileException {
         Optional<Sample> findSample = sampleRepository.findById(sampleId);
         if ( !findSample.isPresent()) {
-            log.error("Sample [{}] doesn't exist", sampleId);
-            return new Response(String.format("Sample [%s] doesn't exist", sampleId), HttpStatus.BAD_REQUEST);
+            throw new SampleNotFoundException(sampleId);
         }
         if (sampleFile.isEmpty()) {
-            log.error("File {} doesn't exist or is empty", sampleFile.getOriginalFilename());
-            return new Response(String.format("File {%s} doesn't exist or is empty", sampleFile.getOriginalFilename()), HttpStatus.BAD_REQUEST);
+            throw new CsvFileException(String.format("File {%s} doesn't exist or is empty", sampleFile.getOriginalFilename()));
         }
 
         Sample sample = findSample.get();
@@ -76,10 +73,7 @@ public class SampleServiceImpl implements SampleService {
             List<SampleSurveyUnit> sampleSurveyUnits = new ArrayList<>();
 
             for (SurveyUnit su : surveyUnits) {
-                SampleSurveyUnit sampleSurveyUnit = new SampleSurveyUnit();
-                sampleSurveyUnit.setSample(sample);
-                sampleSurveyUnit.setSurveyUnit(su);
-                sampleSurveyUnit.setRegisteredDate(new Date());
+                SampleSurveyUnit sampleSurveyUnit = new SampleSurveyUnit(sample, su);
                 sampleSurveyUnits.add(sampleSurveyUnit);
             }
             surveyUnitRepository.saveAll(surveyUnits);
@@ -88,28 +82,29 @@ public class SampleServiceImpl implements SampleService {
             return new Response(String.format("%s surveyUnits created", surveyUnitsDto.size()), HttpStatus.OK);
         }
         catch (IOException e) {
-            log.error("file read error", e);
-            return new Response("file read error", HttpStatus.BAD_REQUEST);
+            throw new CsvFileException("File read error", e);
         }
 
     }
 
     @Override
-    public Response deleteSample(Long sampleId) {
+    public Response deleteSample(Long sampleId) throws SampleNotFoundException {
         Optional<Sample> findSample = sampleRepository.findById(sampleId);
         if ( !findSample.isPresent()) {
-            log.error("Sample [{}] doesn't exist", sampleId);
-            return new Response(String.format("Sample [%s] doesn't exist", sampleId), HttpStatus.BAD_REQUEST);
+            throw new SampleNotFoundException(sampleId);
         }
         List<SampleSurveyUnit> sampleSurveyUnits = sampleSurveyUnitRepository.findBySample(findSample.get());
-        sampleSurveyUnits.stream().filter(ssu -> ssu.getSurveyUnit().getSampleSurveyUnit().size() == 1)
-            .forEach(ssu -> surveyUnitRepository.delete(ssu.getSurveyUnit()));
+        List<SurveyUnit> suWithSingleSsu =
+            sampleSurveyUnits.stream().filter(ssu -> ssu.getSurveyUnit().getSampleSurveyUnit().size() == 1).map(SampleSurveyUnit::getSurveyUnit)
+                .collect(Collectors.toList());
+
         sampleSurveyUnitRepository.deleteAll(sampleSurveyUnits);
+        surveyUnitRepository.deleteAll(suWithSingleSsu);
         return new Response(String.format("sample %s deleted", sampleId), HttpStatus.OK);
     }
 
     @Override
-    public List<SurveyUnit> getSample(Long sampleId) {
+    public List<SurveyUnit> getSurveyUnitsBySample(Long sampleId) {
         Optional<Sample> findSample = sampleRepository.findById(sampleId);
         if (findSample.isPresent()) {
             return surveyUnitRepository.findAllBySample(findSample.get());
@@ -126,6 +121,20 @@ public class SampleServiceImpl implements SampleService {
         sample.setLabel(label);
         sample = sampleRepository.save(sample);
         return new Response(String.format("sample %s create", sample.getId()), HttpStatus.OK);
+    }
+
+    @Override
+    public Sample getSample(Long sampleId) throws SampleNotFoundException {
+        Optional<Sample> findSample = sampleRepository.findById(sampleId);
+        if ( !findSample.isPresent()) {
+            throw new SampleNotFoundException(sampleId);
+        }
+        return findSample.get();
+    }
+
+    @Override
+    public List<Sample> getAllSamples() {
+        return sampleRepository.findAll();
     }
 
 }
