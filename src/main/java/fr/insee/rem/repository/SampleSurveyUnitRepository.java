f3cefd50e6bcab7b5259fd8bdd6ef575c6950fe3
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2022-12-14 12:19:32 +0100

    feat: get list of SU Ids by sample

--- src/main/java/fr/insee/rem/repository/SampleSurveyUnitRepository.java
+++ src/main/java/fr/insee/rem/repository/SampleSurveyUnitRepository.java
@@ -3,6 +3,7 @@ package fr.insee.rem.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fr.insee.rem.entities.Sample;
@@ -12,6 +13,9 @@ import fr.insee.rem.entities.SurveyUnit;
 
 @Repository
 public interface SampleSurveyUnitRepository extends JpaRepository<SampleSurveyUnit, SampleSurveyUnitPK> {
+
+    @Query(value = "select ssu.surveyUnit.id from SampleSurveyUnit ssu where ssu.sample.id = ?1")
+    List<Long> findAllIdsBySample(Long sampleId);
 
     List<SampleSurveyUnit> findBySample(Sample sample);
 
