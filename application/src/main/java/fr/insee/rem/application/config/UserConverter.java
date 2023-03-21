f899e3b7a2bf68c02c187268633c828eded5c4d1
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2023-03-21 16:31:57 +0100

    fix: manage Anonymous Token

--- application/src/main/java/fr/insee/rem/application/config/UserConverter.java
+++ application/src/main/java/fr/insee/rem/application/config/UserConverter.java
@@ -8,10 +8,16 @@ import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 public class UserConverter extends ClassicConverter {
+
+    private static final String ANONYMOUS_USER = "anonymousUser";
+
     @Override
     public String convert(ILoggingEvent event) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
+            if (auth.getPrincipal() != null && ANONYMOUS_USER.equals(auth.getPrincipal())) {
+                return "NOAUTH";
+            }
             return ((Jwt) auth.getCredentials()).getClaims().get("preferred_username").toString().toUpperCase();
         }
         return "";
