82d404f7eccc71113be5c70c9a969007340ea9ad
Mathieu Bergonzini <mathieu.bergonzini@insee.fr>
2023-07-20 13:46:16 +0200

    feat(WIP): add census surveyunits import

--- controller/src/main/java/fr/insee/rem/controller/adapter/CensusAdapter.java
+++ controller/src/main/java/fr/insee/rem/controller/adapter/CensusAdapter.java
@@ -1,13 +1,36 @@
 package fr.insee.rem.controller.adapter;
 
 import fr.insee.rem.controller.sources.CensusSource;
-import fr.insee.rem.domain.dtos.SurveyUnitDto;
+import fr.insee.rem.domain.dtos.*;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CensusAdapter {
     public SurveyUnitDto convert(CensusSource c) {
 
-        return SurveyUnitDto.builder().build();
+        return SurveyUnitDto.builder().externalId(c.getIdinternaute()).context(Context.HOUSEHOLD)
+                .address(convertAddress(c)).persons(convertPersons(c))
+                .additionalInformations(convertAdditionalInformation(c))
+                .externals(c.getExternals()).build();
+    }
+
+    private List<AdditionalInformationDto> convertAdditionalInformation(CensusSource c) {
+        AdditionalInformationDto addInfo = AdditionalInformationDto.builder()
+                .key("identifiantCompte").value(c.getIdentifiant()).build();
+        return List.of(addInfo);
+    }
+
+    private List<PersonDto> convertPersons(CensusSource c) {
+        EmailDto mail = EmailDto.builder().favorite(false).source(Source.INITIAL).mailAddress(c.getMail()).build();
+        PersonDto main = PersonDto.builder().index(1).main(true).externalId(c.getIdenq()).emails(List.of(mail)).build();
+        return List.of(main);
+    }
+
+    private AddressDto convertAddress(CensusSource c) {
+        return AddressDto.builder().streetNumber(c.getNumvoiloc()).repetitionIndex(c.getBisterloc())
+                .streetType(c.getTypevoiloc()).streetName(c.getNomvoiloc()).addressSupplement(c.getResloc())
+                .cityName(c.getCloc()).zipCode(c.getCpostloc()).build();
     }
 }
--- controller/src/main/java/fr/insee/rem/controller/sources/CensusSource.java
+++ controller/src/main/java/fr/insee/rem/controller/sources/CensusSource.java
@@ -1,5 +1,6 @@
 package fr.insee.rem.controller.sources;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CensusSource {
-    private String nothing;
+    private String idinternaute;
+    private String numvoiloc;
+    private String bisterloc;
+    private String typevoiloc;
+    private String nomvoiloc;
+    private String resloc;
+    private String cloc;
+    private String cpostloc;
+    private String idenq;
+    private String mail;
+    private String identifiant;
+    private JsonNode externals;
 }
